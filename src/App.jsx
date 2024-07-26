b52c281d49ac7497cbe8cb1fa601ce77151c5a06
MbaliPuso <mbalipuso@gmail.com>
2024-07-26 12:41:02 +0200

    package installs

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
